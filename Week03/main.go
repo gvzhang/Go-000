e45df82d4e98f92f1b8a582ffc74a772add8b391
zhangguangjian <zhangguangjian@yaowan.com>
2020-12-10 00:08:10 +0800

    work done

--- Week03/main.go
+++ Week03/main.go
@@ -2,43 +2,62 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"golang.org/x/sync/errgroup"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 func main() {
 	ctx := context.Background()
-	quit := make(chan os.Signal)
-	signal.Notify(quit, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-
 	g, eCtx := errgroup.WithContext(ctx)
 	g.Go(func() error {
-		var errChan chan error
-		go func() {
-			errChan <- http.ListenAndServe(":8011", nil)
-		}()
-		select {
-		case <-eCtx.Done():
-			return nil
-		case err := <-errChan:
-			return err
-		}
+		return startServer(ctx, eCtx, ":8015")
 	})
 	g.Go(func() error {
-		select {
-		case <-eCtx.Done():
-			return nil
-		case <-quit:
-			return errors.New("catch exit signal")
-		}
+		return startServer(ctx, eCtx, ":81")
 	})
-
+	g.Go(func() error {
+		return stopSignal(ctx, eCtx)
+	})
 	if err := g.Wait(); err != nil {
-		fmt.Println("errgroup ", err)
+		log.Println("errgroup", err)
+	}
+	log.Println("main exit...")
+}
+
+func startServer(ctx context.Context, egCtx context.Context, addr string) error {
+	errChan := make(chan error, 0)
+	server := &http.Server{Addr: addr, Handler: nil}
+	go func() {
+		errChan <- server.ListenAndServe()
+	}()
+	select {
+	case <-egCtx.Done():
+		log.Println(addr, "http exit...")
+		tCtx, _ := context.WithTimeout(ctx, 10*time.Second)
+		err := server.Shutdown(tCtx)
+		if err != nil {
+			log.Println(addr, "shutdown err", err)
+		}
+		return nil
+	case err := <-errChan:
+		return err
+	}
+}
+
+func stopSignal(ctx context.Context, egCtx context.Context) error {
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	select {
+	case <-egCtx.Done():
+		log.Println("signal exit...")
+		return nil
+	case q := <-quit:
+		return fmt.Errorf("catch exit signal %s", q.String())
 	}
 }
